41f9053c31a88be952b1b0db4f7b29321539b71b
P. Ranjith Reddy <rpreddy1998@gmail.com>
2017-02-10 12:38:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/articleone', function(req,res){
-   res.send('Article two will be served'); 
+app.get('/articleone.html', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'articleone.html')); 
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
